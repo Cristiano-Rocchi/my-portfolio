9da5314924a28e3f943c719b34d9b65b4a4493a9
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-06-14 22:42:34 +0200

    Home Complete

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import "./Landing.scss";
 import "../../App.scss";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import HomeImg from "../../Assets/Img/Home/landing-img.png";
+import ContactImg from "../../Assets/Img/Home/contactImg.png";
 // Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, FreeMode } from "swiper/modules";
 // Loghi
 import html5 from "../../Assets/Img/Loghi/html5.png";
 import css3 from "../../Assets/Img/Loghi/css3.png";
 import js from "../../Assets/Img/Loghi/js.png";
 import ts from "../../Assets/Img/Loghi/ts.png";
+import Rct from "../../Assets/Img/Loghi/react.png";
+import Redux from "../../Assets/Img/Loghi/redux.png";
+import Sass from "../../Assets/Img/Loghi/sass.png";
+import Java from "../../Assets/Img/Loghi/java.png";
+import Spring from "../../Assets/Img/Loghi/spring.png";
+import Maven from "../../Assets/Img/Loghi/maven.png";
+import Pg from "../../Assets/Img/Loghi/pg.png";
+import Github from "../../Assets/Img/Loghi/github.png";
+import Git from "../../Assets/Img/Loghi/git.png";
+import Jest from "../../Assets/Img/Loghi/jest.png";
+import jwt from "../../Assets/Img/Loghi/jwt.png";
+import Bstrap from "../../Assets/Img/Loghi/bootstrap.png";
+import Hibernate from "../../Assets/Img/Loghi/hibernate.png";
+import Cloudinary from "../../Assets/Img/Loghi/cloudinary.png";
+import Backblaze from "../../Assets/Img/Loghi/backblaze.png";
+import Njs from "../../Assets/Img/Loghi/njs.png";
+import ReactRouter from "../../Assets/Img/Loghi/react-router.png";
+import Netlify from "../../Assets/Img/Loghi/netlify.png";
+import Vercel from "../../Assets/Img/Loghi/vercel.png";
+import Figma from "../../Assets/Img/Loghi/figma.png";
+//svg
+import In from "../../Assets/Svg/in.svg";
+import Phone from "../../Assets/Svg/phone.svg";
+import Arrow from "../../Assets/Svg/arrow.svg";
 
 const Landing = () => {
   return (
@@ -36,7 +61,10 @@ const Landing = () => {
             >
               <div className="card-landing p-3">
                 <div className="d-flex justify-content-between mb-2">
-                  <h5 className="fw-bold">SITI CHE GESTISCO</h5> <h4>+</h4>
+                  <h5 className="fw-bold">SITI CHE GESTISCO</h5>{" "}
+                  <div>
+                    <img src={Arrow} alt="" />
+                  </div>
                 </div>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
@@ -46,7 +74,10 @@ const Landing = () => {
               <div className="card-landing p-3 mb-3">
                 {" "}
                 <div className="d-flex justify-content-between mb-2">
-                  <h5 className="fw-bold">I MIEI PROGETTI</h5> <h4>+</h4>
+                  <h5 className="fw-bold">I MIEI PROGETTI</h5>{" "}
+                  <div>
+                    <img src={Arrow} alt="" />
+                  </div>
                 </div>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
@@ -54,18 +85,21 @@ const Landing = () => {
                 </p>
               </div>
             </Col>
-          </Row>{" "}
+          </Row>
           {/* -----CAROSELLO------ */}
           <Row>
             <Col xs={12}>
               <Swiper
-                slidesPerView={9}
+                slidesPerView="10"
                 spaceBetween={30}
+                loop={true}
+                freeMode={true}
+                speed={4000}
                 autoplay={{
-                  delay: 3000, // tempo in millisecondi tra ogni slide
-                  disableOnInteraction: false, // continua anche se l'utente interagisce
+                  delay: 0,
+                  disableOnInteraction: false,
                 }}
-                modules={[Autoplay]}
+                modules={[Autoplay, FreeMode]}
                 className="my-swiper"
               >
                 <SwiperSlide>
@@ -80,16 +114,109 @@ const Landing = () => {
                 <SwiperSlide>
                   <img className="w-50" src={ts} alt="" />
                 </SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
-                <SwiperSlide>Slide 10</SwiperSlide>{" "}
-                <SwiperSlide>Slide 11</SwiperSlide>{" "}
-                <SwiperSlide>Slide 12</SwiperSlide>{" "}
-                <SwiperSlide>Slide 13</SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Rct} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Redux} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Sass} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Java} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Spring} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Maven} alt="" />
+                </SwiperSlide>{" "}
+                <SwiperSlide>
+                  <img className="w-50" src={Pg} alt="" />
+                </SwiperSlide>{" "}
+                <SwiperSlide>
+                  <img className="w-50" src={Github} alt="" />
+                </SwiperSlide>{" "}
+                <SwiperSlide>
+                  <img className="w-50" src={Git} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Bstrap} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Hibernate} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Cloudinary} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Backblaze} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Njs} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Vercel} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Jest} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={jwt} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={ReactRouter} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Netlify} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img className="w-50" src={Figma} alt="" />
+                </SwiperSlide>
               </Swiper>
+            </Col>
+          </Row>
+
+          <Row>
+            {/* -----CONTATTI------ */}
+            <Col xs={12} md={8}>
+              <div className="contatti-card p-3  mb-4">
+                <div className="d-flex justify-content-between">
+                  <div>
+                    <h4 className="fw-bold mb-4">CONTATTI</h4>
+                    <div className="d-flex gap-4">
+                      <Button>
+                        Posta Elettronica
+                        <img className="ms-2 mb-1" src={Arrow} alt="" />
+                      </Button>
+                      <img className="ms-3" src={In} alt="Linkedin" />
+                      <img src={Phone} alt="Phone" />
+                    </div>
+                  </div>
+                  <div className="me-5">
+                    <img src={ContactImg} alt="" />
+                  </div>
+                </div>
+              </div>
+            </Col>
+            {/* -----ABOUT ME------ */}
+            <Col xs={12} md={4}>
+              {" "}
+              <div className="about-card p-3">
+                <div className="d-flex justify-content-between">
+                  <h4 className="fw-bold mb-2">ABOUT ME</h4>{" "}
+                  <div>
+                    <img src={Arrow} alt="" />
+                  </div>
+                </div>
+
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+                  officia pariatur quam architecto obcaecati, cumque
+                  voluptatibus,
+                </p>
+              </div>
             </Col>
           </Row>
         </Container>
